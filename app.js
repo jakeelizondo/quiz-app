62537f511557878360f762726113c53b19b09fee
Jake Elizondo <jake.elizondo23@gmail.com>
2020-11-18 14:31:07 -0500

    added shuffle function on quiz start

--- app.js
+++ app.js
@@ -295,6 +295,7 @@ function handleQuizStart() {
   $('main').on('click', '#startQuiz', function (event) {
     // set the quiz started in the store to true
     store.quizStarted = true;
+    shuffle(store.questions);
 
     // render a new quiz
     renderQuiz();
@@ -346,4 +347,26 @@ function handleNewQuizReset() {
 
     resetQuiz();
   });
+}
+
+// SHUFFLE ARRAY
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
